eddc9d1a857cb98270bd92c081d864dbcacf61d6
Koopa1018 <caleblovestoytrains@gmail.com>
2021-02-26 07:55:39 -0700

    Try to make MovingPlatformHandler work vertically.
    My efforts here include: displacing the raycast with the character's velocity (hmm, this isn't good to have for going down--I think it still works down, though?); laying groundwork to have a more proper velocity-tracking-based system, which will probably have the pleasant side effect of reducing GetComponent calls per-frame (on average, anyway--still gotta use it some but).
    
    Gonna lay that out real quick: on detect platform with velocity, that platform's Velocity is stored, then _unless a new platform is detected_ that's _confirmed intercepting_ (ooh, should allow configure intercept-allowed or not! But that'd be best if it _only affects semi-solids_), or the character is _confirmed_ no longer on the platform (which should throw an event so we can have X-velocity follow into the air movement if you'd like it to be that way), strong movement should just duplicate the movement of the platform.

--- Scripts/Collisions/MovingPlatformHandler.cs
+++ Scripts/Collisions/MovingPlatformHandler.cs
@@ -1,3 +1,24 @@
+// Deciding which movements should prevail? It actually depends on whether the _platform_ is parallel or perpendicular to the wall.
+		
+// Say we've got a platform moving under a moving (or, heck, static) wall:
+//    [[[
+//   o[[[
+//  >>>
+// The wall's movement (or lack thereof) must prevail, and the player must be forced off the platform.
+// Thusly: we let the X movement from horizontal casts _replace_ X movement from vertical casts.
+
+// Say the player is wallhanging on a vertically-moving wall and hits the same situation:
+// [[[[^
+//    o^
+//     ^
+// This could be a situation with a number of different outcomes. In Sonic & Knuckles-style
+// wallhanging, the player would be forced to drop off; in Mega Man X-style hanging, the
+// player would be forced gradually downward as in the horizontal example above.
+
+// =====tl;dr Perpendicular platforms invariably lose to the wall.
+
+//Parallel platforms, on the other hand, will crush the player, so they just always callback out to some other system.
+
 using UnityEngine;
 using Unity.Mathematics;
 
@@ -5,15 +26,24 @@ using Clouds.Movement2D;
 using Clouds.Raycasting;
 
 namespace Clouds.Platformer.Character {
+	
+	
 	public class MovingPlatformHandler : MonoBehaviour {
 		[SerializeField] bool _supportMovingFloors = true;
 		internal bool SupportMovingFloors => _supportMovingFloors;
+		
 		[SerializeField] bool _supportMovingWalls = false;
 		internal bool SupportMovingWalls => _supportMovingWalls;
+
 		[Space]
+
 		[SerializeField] bool breakWallClimbOnWallEmergence = true;
 		[SerializeField] bool breakWallClimbOnHitFloor = true;
 		[SerializeField] bool breakWallClimbOnHitCeiling = true;
+
+
+		public float2 speed => math.select(weakSpeed, strongSpeed, strongSpeedUsed);
+		
 		/// <summary>
 		/// Accumulated movement that, in the event of conflict, replaces weak movement.
 		/// </summary>
@@ -27,76 +57,22 @@ namespace Clouds.Platformer.Character {
 		/// </summary>
 		bool2 strongSpeedUsed = false;
 
-		public float2 speed => math.select(weakSpeed, strongSpeed, strongSpeedUsed);
 
 		Collider2D strongCollider, weakCollider;
-
-		// Deciding which movements should prevail? It actually depends on whether the _platform_ is parallel or perpendicular to the wall.
-		
-		// Say we've got a platform moving under a moving (or, heck, static) wall:
-		//    [[[
-		//   o[[[
-		//  >>>
-		// The wall's movement (or lack thereof) must prevail, and the player must be forced off the platform.
-		// Thusly: we let the X movement from horizontal casts _replace_ X movement from vertical casts.
-		
-		// Say the player is wallhanging on a vertically-moving wall and hits the same situation:
-		// [[[[^
-		//    o^
-		//     ^
-		// This could be a situation with a number of different outcomes. In Sonic & Knuckles-style
-		// wallhanging, the player would be forced to drop off; in Mega Man X-style hanging, the
-		// player would be forced gradually downward as in the horizontal example above.
-
-		// =====tl;dr Perpendicular platforms invariably lose to the wall.
-		
-		//Parallel platforms, on the other hand, will crush the player, so they just always callback out to some other system.
-
-		float2 valueOf (Velocity velocity) {
-			if (velocity == null) {
-				return 0;
-			} else {
-				return velocity.Value;
-			}
-		}
-
-		/// <summary>
-		/// Is this character moving toward the wall?
-		/// </summary>
-		/// <param name="myCurrentSpeed">The speed I'm going. Should we factor the character's manual speed? I don't know.</param>
-		/// <param name="wallSpeed">Speed the wall is going.</param>
-		/// <returns><c>TRUE</c> if this character is moving towards the wall; <c>FALSE</c> otherwise.</returns>
-		int compareVelocities (float myCurrentSpeed, float wallSpeed) {
-			//So we can rotate everything so my movement is forwards.
-			float mySpeedSign = math.sign(myCurrentSpeed);
-
-			//Rotated so current movement is 1, is wall movement less than my current movement?
-			return (wallSpeed * mySpeedSign).CompareTo(myCurrentSpeed * mySpeedSign);
-		}
-
-		/// <summary>
-		/// Caches the collider used to provide weak movement.
-		/// </summary>
-		/// <param name="candidate">The current collider we're checking.</param>
-		void assignWeakCollider (Collider2D candidate) {
-			//If we've got no weak collider, assign this one, I guess.
-			if (weakCollider == null) {
-				weakCollider = candidate;
-			}
-			//If we've got one, re-evaluate if it's the right one.
-			else {
-				//@TODO: Select whether to favor same-direction or different-direction,
-				//whether to favor higher speed or lower speed or both,
-				//and of course don't even check for new ones if it's not desired.
-			}
-		}
+		Velocity strongVelocity, weakVelocity;
 
 		/// <summary>
 		/// Find the displacement that ought to have occurred from moving platforms.
 		/// </summary>
 		/// <param name="velocity"></param>
 		/// <returns></returns>
-		internal float2 HandleMovingPlatforms(CharaCasterDimensions dimensions, float skinWidth, Collider2D collider, float2 velocity, int facingX, LayerMask hMask, LayerMask vMask) {
+		internal float2 HandleMovingPlatforms(CharaCasterDimensions dimensions, float skinWidth, Collider2D myCollider, float2 velocity, int facingX, LayerMask hMask, LayerMask vMask) {
+			//We want to displace the origin of the collision cast by our currently-ridden platform's velocity,
+			//assuming we have one, so we need to read that value.
+			float2 castCtr = valueOf(strongVelocity);
+			castCtr += dimensions.BoxEpicenter();
+
+
 			//To store raycasts in.
 			RaycastHit2D[] veloCheck = new RaycastHit2D[2];
 
@@ -107,7 +83,7 @@ namespace Clouds.Platformer.Character {
 			if (SupportMovingWalls) {
 				//Check for moving platforms forwards.
 				Physics2D.BoxCastNonAlloc(
-					dimensions.BoxEpicenter(), //Center
+					castCtr, //Center
 					dimensions.Size, //Size
 					0, //Rotation/angle
 					Vector2.right * facingX, //diracion
@@ -115,7 +91,7 @@ namespace Clouds.Platformer.Character {
 					math.abs(velocity.x) + skinWidth, //distance
 					hMask //layer mask
 				);
-				veloCheck[0] = veloCheck.FirstNonSelfHit(collider);
+				veloCheck[0] = veloCheck.FirstNonSelfHit(myCollider);
 
 				if (veloCheck[0]) {
 					TakeSpeedX(veloCheck[0].collider, /*isWallSliding*/false);
@@ -127,7 +103,7 @@ namespace Clouds.Platformer.Character {
 			if (SupportMovingFloors) {
 				//Cast a thin (skinWidth)-sized box to find a floor.
 				Physics2D.BoxCastNonAlloc(
-					dimensions.BoxEpicenter(), //dimensions.BottomCenter(skinWidth) + new float2(0, skinWidth/2), //Center
+					castCtr, //dimensions.BottomCenter(skinWidth) + new float2(0, skinWidth/2), //Center
 					dimensions.Size, //new float2(dimensions.Size.x, skinWidth), //Size*/
 					0, //Rotation/angle
 					Vector2.up * math.sign(velocity.y), //diracion
@@ -135,7 +111,7 @@ namespace Clouds.Platformer.Character {
 					math.abs(velocity.y) + skinWidth, //distance
 					vMask //layer mask
 				);
-				veloCheck[0] = veloCheck.FirstNonSelfHit(collider);
+				veloCheck[0] = veloCheck.FirstNonSelfHit(myCollider);
 
 				if (veloCheck[0]) {
 					TakeSpeedY(
@@ -144,7 +120,6 @@ namespace Clouds.Platformer.Character {
 					);
 				}
 			}
-
 
 			return FinalizePlatformMovement();
 		}
@@ -159,15 +134,17 @@ namespace Clouds.Platformer.Character {
 				return;
 			}
 			
-			float2 wallVelocity = valueOf(candidate.GetComponent<Velocity>());
+			Velocity candidateVelocity = candidate.GetComponent<Velocity>();
+			float2 wallVelocity = valueOf(candidateVelocity);
 			
 			//If this platform is coming towards us (from our perspective), it's meant to override our current speed!
 			if (compareVelocities(speed.x, wallVelocity.x) == -1) {
 				//Save this collider as the one we're under the effects of.
 				strongCollider = candidate;
+				strongVelocity = candidateVelocity;
 
 				//Use its velocity.
-				strongSpeedUsed.x = true;
+				strongSpeedUsed.x = candidateVelocity != null;
 				strongSpeed.x = wallVelocity.x;
 
 				if (breakWallClimbOnWallEmergence) {
@@ -230,17 +207,57 @@ namespace Clouds.Platformer.Character {
 			}
 		}
 
+		internal float2 FinalizePlatformMovement () {
+			float2 returner = speed;
+			resetState();
+			return returner;
+		}
+
+		/// <summary>
+		/// Is this character moving toward the wall?
+		/// </summary>
+		/// <param name="myCurrentSpeed">The speed I'm going. Should we factor the character's manual speed? I don't know.</param>
+		/// <param name="wallSpeed">Speed the wall is going.</param>
+		/// <returns><c>TRUE</c> if this character is moving towards the wall; <c>FALSE</c> otherwise.</returns>
+		int compareVelocities (float myCurrentSpeed, float wallSpeed) {
+			//So we can rotate everything so my movement is forwards.
+			float mySpeedSign = math.sign(myCurrentSpeed);
+
+			//Rotated so current movement is 1, is wall movement less than my current movement?
+			return (wallSpeed * mySpeedSign).CompareTo(myCurrentSpeed * mySpeedSign);
+		}
+
+		/// <summary>
+		/// Caches the collider used to provide weak movement.
+		/// </summary>
+		/// <param name="candidate">The current collider we're checking.</param>
+		void assignWeakCollider (Collider2D candidate) {
+			//If we've got no weak collider, assign this one, I guess.
+			if (weakCollider == null) {
+				weakCollider = candidate;
+			}
+			//If we've got one, re-evaluate if it's the right one.
+			else {
+				//@TODO: Select whether to favor same-direction or different-direction,
+				//whether to favor higher speed or lower speed or both,
+				//and of course don't even check for new ones if it's not desired.
+			}
+		}
+
+		float2 valueOf (Velocity velocity) {
+			if (velocity == null) {
+				return 0;
+			} else {
+				return velocity.Value;
+			}
+		}
+
 		void resetState () {
 			strongCollider = weakCollider = null;
 			strongSpeed = weakSpeed = 0;
 			strongSpeedUsed = false;
 		}
 
-		internal float2 FinalizePlatformMovement () {
-			float2 returner = speed;
-			resetState();
-			return returner;
-		}
 
 	}
 }
